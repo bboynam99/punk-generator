346b6f48b070d32a7bc95731be40d4e38d42cdae
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-10-23 20:59:48 +0700

    Added generate punk

--- src/main/scala/cipherdogs/Main.scala
+++ src/main/scala/cipherdogs/Main.scala
@@ -1,5 +1,27 @@
 package cipherdogs
 
+import java.awt.Color
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.imageio.ImageIO
+
 object Main extends App {
-  println("Hello, world!")
+  println("Starting")
+  val canvas = new BufferedImage(24, 24, BufferedImage.TYPE_INT_RGB);
+  val g      = canvas.getGraphics;
+
+  g.setColor(Color.WHITE);
+  g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+  val parts: List[String] = List("face", "mouth", "nose", "eyes", "ears", "beard", "hair");
+
+  parts
+    .zip(0 until parts.size)
+    .foreach(tuple => {
+      val image: Image = new ImageIcon(getClass.getResource("/parts/" + tuple._1 + "/" + tuple._1 + "2.png")).getImage;
+      g.drawImage(image, 0, 0, 24, 24, null);
+    });
+
+  ImageIO.write(canvas, "png", new java.io.File("punk.png"));
 }
